e122cae180c61c8233542ec47cdd08c52a1374b7
lanbreak <940019689@qq.com>
2022-08-31 00:50:47 +0800

    eureka-server

---
+++ springcloud-eureka-server-03/src/main/java/com/wkcto/springcloud/Application8761.java
@@ -0,0 +1,14 @@
+package com.wkcto.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application8761 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application8761.class);
+    }
+}
---
+++ springcloud-eureka-server-03/src/test/java/com/wkcto/springcloud/SpringcloudEurekaServer03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkcto.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudEurekaServer03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
